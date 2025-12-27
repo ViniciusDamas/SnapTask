3dd9ab15c1dc475154880fe71a5824e30dfd5501
ViniciusDamas <vinicius.damas@hotmail.com>
2025-12-27 11:56:16 -0800

    feat(domain) add DateTime field

--- backend/SnapTaskApi/Domain/Entities/Board.cs
+++ backend/SnapTaskApi/Domain/Entities/Board.cs
@@ -4,6 +4,7 @@ public class Board
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public List<Column> Columns { get; set; } = new();
 }
