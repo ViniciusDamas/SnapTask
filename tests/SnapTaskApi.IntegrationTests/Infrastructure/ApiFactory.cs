fffc1815764972b740865173eb4841ecb78c1d9b
ViniciusDamas <vinicius.damas@hotmail.com>
2026-02-02 23:31:07 -0800

    add integration tests dependencies

---
+++ tests/SnapTaskApi.IntegrationTests/Infrastructure/ApiFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SnapTaskApi.Infrastructure.Persistence;
+
+namespace SnapTaskApi.IntegrationTests.Infrastructure;
+
+public class ApiFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll<DbContextOptions<AppDbContext>>();
+
+            var dbName = $"SnapTask_TestDb_{Guid.NewGuid():N}";
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(dbName);
+            });
+        });
+    }
+}
